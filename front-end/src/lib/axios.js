ad9c498a93c0aaa26cbaa4057d6dee920d5400d8
Alphonse Mwaka Lukombo <Mwaka3008@gmail.com>
2026-05-05 17:17:58 +0200

    Url's reviews

--- front-end/src/lib/axios.js
+++ front-end/src/lib/axios.js
@@ -6,7 +6,7 @@ config();
 
 
 
-const BASE_URL = process.env.REACT_APP_URL+'api' || "http://localhost:5000/api";
+const BASE_URL = "https://pizzaria-realtime-2.onrender.com/api" || "http://localhost:5000/api";
 
 export const axiosInstance = axios.create({
     baseURL:`${BASE_URL}`,
